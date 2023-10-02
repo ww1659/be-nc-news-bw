5515609b78a2eea5e86afc02afa997717dcdec78
Billy White <billywhite@Billys-Mac-mini.local>
2023-10-02 14:18:22 +0100

    Updated controller for endpoints

--- controllers/endpoints-controller.js
+++ controllers/endpoints-controller.js
@@ -1,13 +1,6 @@
-const { readFile } = require("fs/promises");
+const endPoints = require("../endpoints.json");
 
 exports.getEndpoints = (req, res, next) => {
-  readFile("./endpoints.json")
-    .then((file) => {
-      const endPoints = JSON.parse(file);
-      delete endPoints[Object.keys(endPoints)[0]];
-      res.status(200).send({ endPoints });
-    })
-    .catch((err) => {
-      next(err);
-    });
+  delete endPoints[Object.keys(endPoints)[0]];
+  res.status(200).send({ endPoints });
 };
