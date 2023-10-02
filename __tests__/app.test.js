20e17d5b422a49c2b2c8af8b8b6b6182753b4e30
Billy White <billywhite@Billys-Mac-mini.local>
2023-10-02 14:43:19 +0100

    Task 4 complete: get Article by Id

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -9,6 +9,26 @@ beforeEach(() => {
 });
 afterAll(() => db.end());
 
+describe("api", () => {
+  test("GET:200 returns an object of available endpoints", () => {
+    return request(app)
+      .get("/api")
+      .expect(200)
+      .then((response) => {
+        const endPoints = response.body.endPoints;
+        for (const endpoint in endPoints) {
+          expect(typeof endPoints[endpoint].description).toBe("string");
+          expect(Array.isArray(endPoints[endpoint].queries)).toBe(true);
+          expect(
+            typeof endPoints[endpoint].exampleResponse &&
+              endPoints[endpoint].exampleResponse !== null &&
+              !Array.isArray(endPoints[endpoint].exampleResponse)
+          ).toBe(true);
+        }
+      });
+  });
+});
+
 describe("api/topics", () => {
   test("GET:200 returns an array of topic objects with properties of 'slug' and 'description'", () => {
     return request(app)
@@ -25,5 +45,44 @@ describe("api/topics", () => {
   });
   test("GET:404 returns status 404 for a non-existent path", () => {
     return request(app).get("/api/topic").expect(404);
+  });
+});
+
+describe("api/articles/:article_id", () => {
+  test("GET:200 returns an article object with corresponding properties", () => {
+    return request(app)
+      .get("/api/articles/3")
+      .expect(200)
+      .then((response) => {
+        const article = response.body.article;
+        article.forEach((article) => {
+          expect(article.title).toBe("Eight pug gifs that remind me of mitch");
+          expect(article.topic).toBe("mitch");
+          expect(article.author).toBe("icellusedkars");
+          expect(article.body).toBe("some gifs");
+          expect(article.hasOwnProperty("created_at")).toBe(true);
+          expect(article.article_img_url).toBe(
+            "https://images.pexels.com/photos/158651/news-newsletter-newspaper-information-158651.jpeg?w=700&h=700"
+          );
+          expect(article.votes).toBe(0);
+          expect(article.article_id).toBe(3);
+        });
+      });
+  });
+  test("GET:404 returns status 404 and error message for a valid but non-existent article id", () => {
+    return request(app)
+      .get("/api/articles/19")
+      .expect(404)
+      .then((response) => {
+        expect(response.body.msg).toBe("Article does not exist!");
+      });
+  });
+  test("GET:400 returns status 400 and error message for an invalid article id", () => {
+    return request(app)
+      .get("/api/articles/dog")
+      .expect(400)
+      .then((response) => {
+        expect(response.body.msg).toBe("Invalid Article Id");
+      });
   });
 });
--- app.js
+++ app.js
@@ -1,8 +1,30 @@
 const express = require("express");
 const app = express();
 const { getTopics } = require("./controllers/topics-controller");
+const { getEndpoints } = require("./controllers/endpoints-controller");
+const { getArticleById } = require("./controllers/articles-controller.js");
+
+app.get("/api", getEndpoints);
 
 app.get("/api/topics", getTopics);
+
+app.get("/api/articles/:article_id", getArticleById);
+
+// psql errors
+app.use((err, req, res, next) => {
+  if (err.code === "22P02") {
+    res.status(400).send({ msg: "Invalid Article Id" });
+  }
+  next(err);
+});
+
+//custom errors
+app.use((err, req, res, next) => {
+  if (err.status) {
+    res.status(err.status).send({ msg: err.msg });
+  }
+  next(err);
+});
 
 //path doesn't exist error handling
 app.use((err, req, res, next) => {
